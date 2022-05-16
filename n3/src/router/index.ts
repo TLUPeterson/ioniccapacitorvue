8a834ea1fcacda1ef176df832d02e82ac9ce71a7
Margen Peterson <margen@tlu.ee>
2022-05-16 18:52:58 +0300

    Removed non-necessary files and lines

--- n3/src/router/index.ts
+++ n3/src/router/index.ts
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
 import PageTwo from '../views/PageTwo.vue';
-import ProfilePage from '../views/ProfilePage.vue';
 
 
 const routes: Array<RouteRecordRaw> = [
@@ -10,15 +8,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     redirect: '/tasks'
   },
-  /*{
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  },
-  {
-    path: "/kanye",
-    component: ProfilePage,
-  },*/
   {
     path: "/tasks",
     component: PageTwo,
